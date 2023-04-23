4377e41878515bd42dab9b811579cec7a6869ab5
Abhigyan Dutta <abhigyandutta72@gmail.com>
2023-04-24 00:30:28 +0530

    initial commit

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=3;
+    int b=4;
+    if(a>b)
+    {
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+
+
+}
